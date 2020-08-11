ea7b32de8f677c68ff5dfe48b9428e4202d55663
DanTsvirkun <danila.tsvirkun555@gmail.com>
2020-08-11 22:51:25 +0300

    basic settings

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import "./index.css";
+import App from "./components/App/App";
+import store, { persistor } from "./redux/store";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+  document.getElementById("root")
+);
