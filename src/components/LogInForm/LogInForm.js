9d2598685a79260626e28fbb00eba62b648363a4
Andrii Kulyk <kyluk5@gmail.com>
2020-08-13 22:15:26 +0300

    fixing problem with background

--- src/components/LogInForm/LogInForm.js
+++ src/components/LogInForm/LogInForm.js
@@ -3,22 +3,16 @@ import styles from "./LogInForm.module.css";
 
 const LogInForm = () => {
   return (
-    <div className={styles.auth_wrapper}>
-      <form className={styles.auth}>
-        <h1 className={styles.auth_title}>Вхід</h1>
-        <input
-          type="email"
-          placeholder="E-mail"
-          className={styles.auth_input}
-        />
-        <input
-          type="password"
-          placeholder="Пароль"
-          className={styles.auth_input}
-        />
-        <button className={styles.auth_btn}>Увійти</button>
-      </form>
-    </div>
+    <form className={styles.auth}>
+      <h1 className={styles.auth_title}>Вхід</h1>
+      <input type="email" placeholder="E-mail" className={styles.auth_input} />
+      <input
+        type="password"
+        placeholder="Пароль"
+        className={styles.auth_input}
+      />
+      <button className={styles.auth_btn}>Увійти</button>
+    </form>
   );
 };
 
