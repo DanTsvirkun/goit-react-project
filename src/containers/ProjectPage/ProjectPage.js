8b07d077d4cf31aa937f200656090542155f3e4b
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-08-13 21:29:47 +0300

    SBC-20 done

--- src/containers/ProjectPage/ProjectPage.js
+++ src/containers/ProjectPage/ProjectPage.js
@@ -2,37 +2,43 @@ import React from "react";
 import styles from "./ProjectPage.module.css";
 import pencil from "../../Icons/pencil.svg";
 import plus from "../../Icons/plus.svg";
+import SprintSidebar from "../../components/SprintSidebar/SprintSidebar";
 
 const ProjectPage = () => {
   return (
-    // Header start///
     <>
-      {/* Header end*/}
-      <div className={styles.projectWrapper}>
-        <div className={styles.project__header__wrapper}>
-          <div className={styles.project__button__wrapper}>
-            <h1 className={styles.project__header}>Project 1</h1>
-            <button className={`${styles.button} ${styles.pencil}`}>
-              <img
-                className={`${styles.Icon}`}
-                src={pencil}
-                alt="pencil"
-                height="20"
-                width="20"
-              />
-            </button>
-          </div>
-          <div className={styles.btnWrapper}>
-            <button className={styles.button}>
-              <img
-                className={`${styles.Icon}`}
-                src={plus}
-                alt="pencil"
-                height="44"
-                width="44"
-              />
-            </button>
-            <p className={styles.sprintTxt}>Створити спринт</p>
+      <div className={styles.page_wrapper}>
+        {/* <SprintSidebar /> */}
+        <div className={styles.projectWrapper}>
+          <div className={styles.project__header__wrapper}>
+            <div
+              className={`${styles.project__button__wrapper} ${styles.project__wrapper}`}
+            >
+              <h1 className={styles.project__header}>Project 1</h1>
+              <button className={`${styles.button} ${styles.button__pencil}`}>
+                <img
+                  className={`${styles.icon}`}
+                  src={pencil}
+                  alt="pencil"
+                  height="20"
+                  width="20"
+                />
+              </button>
+            </div>
+            <div
+              className={`${styles.project__button__wrapper} ${styles.project__wrapper}`}
+            >
+              <button className={styles.button}>
+                <img
+                  className={`${styles.icon}`}
+                  src={plus}
+                  alt="pencil"
+                  height="44"
+                  width="44"
+                />
+              </button>
+              <p className={styles.sprint_text}>Створити спринт</p>
+            </div>
           </div>
         </div>
       </div>
