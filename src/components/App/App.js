5a7f30dae507ff1bee5b8e773d99aa4d5952b4fa
DanTsvirkun <danila.tsvirkun555@gmail.com>
2020-08-13 16:58:27 +0300

    before pulling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,8 +9,8 @@ const App = () => {
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <Header />
-        <SprintPage />
-        {/* <Registration /> */}
+        {/* <SprintPage /> */}
+        <Registration />
       </div>
     </div>
   );
