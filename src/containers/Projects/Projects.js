2524db9c162ee7c593c5732b439ca0062d34a0bb
Mypaxa4e <ivanshtypula@gmail.com>
2020-08-13 17:55:26 +0300

    SBC-11

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ProjectsPageList from "../../components/ProjectsPageList/ProjectsPageList";
 
-import projectStyles from "./Projects.module.css"
+import projectStyles from "./Projects.module.css";
 
 class Projects extends Component {
   render() {
     return (
-      <div className={projectStyles.headBar}>
-        <h1 className={projectStyles.headBarTitle}>Проекти</h1>
-        
-        <label  className={projectStyles.headBarLabel}>
-        <button name="createProject" className={projectStyles.headBarBtn}>+</button>
-          Створити проект</label>
-      </div>
+      <>
+        <div className={projectStyles.headBar}>
+          <h1 className={projectStyles.headBarTitle}>Проекти</h1>
+
+          <label className={projectStyles.headBarLabel}>
+            <button name="createProject" className={projectStyles.headBarBtn}>
+              +
+            </button>
+            Створити проект
+          </label>
+        </div>
+        <ProjectsPageList />
+      </>
     );
   }
 }
