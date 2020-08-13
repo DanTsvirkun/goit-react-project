05860d729379c38e87cad5b24b0cfd6ffb58bfb7
Andrii Kulyk <kyluk5@gmail.com>
2020-08-13 10:44:42 +0300

    init

---
+++ src/containers/Registaration/Registaration.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import styles from "./Registaration.module.css";
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
+
+const Registaration = () => {
+  return (
+    <>
+      <RegistrationForm />;
+    </>
+  );
+};
+
+export default Registaration;
