e7c914ab4708a65c9e997877a0b48f29ac156548
PopeyeIT <igorserov28@gmail.com>
2020-08-13 15:16:43 +0300

    ad test route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import styles from './App.module.css';
-import SprintPage from '../../containers/Sprint/Sprint';
+import React from "react";
+import styles from "./App.module.css";
+import SprintPage from "../../containers/Sprint/Sprint";
 
 const App = () => {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <SprintPage />
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.container}>
+				<SprintPage />
+			</div>
+		</div>
+	);
 };
 
 export default App;
+<Rote
+	path="/projects/:projecId/sprints"
+	component={SprintPage}
+/>;
 
 // http://localhost:3000/projects/project/sprints?sprint=1
+// /projects/:projecId/sprints/:sprintId
+// projects/123123jdkfajfka/sprints/fjkaojks123?search=task
