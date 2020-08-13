0418a0844f41ef78a3e6223f2286e2daf7b440da
Andrii Kulyk <kyluk5@gmail.com>
2020-08-13 14:28:14 +0300

    problem vith opacity of ball

--- src/containers/Registaration/Registaration.js
+++ src/containers/Registaration/Registaration.js
@@ -8,20 +8,24 @@ import elipse3 from "./images/Ellipse 3.svg";
 import elipse4 from "./images/Ellipse 4.svg";
 import elipse5 from "./images/Ellipse 5.svg";
 import elipse6 from "./images/Ellipse 6.svg";
-import elipse65_white from "./images/Rectangle65white.png";
+import union_white from "./images/Union.white.svg";
+import union_orange from "./images/Union.orange.svg";
 
 const Registaration = () => {
   return (
-    <div className={styles.registration_wrapper}>
-      <img src={elipse1} alt="298" className={styles.elipse1} />
-      <img src={elipse2} alt="252" className={styles.elipse2} />
-      <img src={elipse3} alt="118" className={styles.elipse3} />
-      <img src={elipse4} alt="48" className={styles.elipse4} />
-      <img src={elipse5} alt="78" className={styles.elipse5} />
-      <img src={elipse6} alt="26" className={styles.elipse6} />
-      <img src={elipse65_white} alt="430" />
-      <RegistrationForm />;
-    </div>
+    <>
+      <div className={styles.test_header}>Test version of header</div>
+
+      <img src={elipse1} alt="" className={styles.elipse1} />
+      <img src={elipse2} alt="" className={styles.elipse2} />
+      <img src={elipse3} alt="" className={styles.elipse3} />
+      <img src={elipse4} alt="" className={styles.elipse4} />
+      <img src={elipse5} alt="" className={styles.elipse5} />
+      <img src={elipse6} alt="" className={styles.elipse6} />
+      <img src={union_white} alt="" className={styles.union_white} />
+      <img src={union_orange} alt="" className={styles.union_orange} />
+      <RegistrationForm />
+    </>
   );
 };
 
