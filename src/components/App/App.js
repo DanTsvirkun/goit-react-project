1a371a099fbea57a93fdb0400f8bd9039412ebcf
Mypaxa4e <ivanshtypula@gmail.com>
2020-08-13 12:03:18 +0300

    page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import styles from "./App.module.css";
 
+//
+import Projects from "../../containers/Projects/Projects.js"
+//
+
 const App = () => {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.container}></div>
+      <div className={styles.container}>
+        <Projects/>
+      </div>
     </div>
   );
 };
--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+import projectStyles from "./Projects.module.css"
+
 class Projects extends Component {
   render() {
     return (
-      <div>
+      <div className={projectStyles.headBar}>
+        <h1 className={projectStyles.headBarTitle}>Проекти</h1>
         
+        <label  className={projectStyles.headBarLabel}>
+        <button name="createProject" className={projectStyles.headBarBtn}>+</button>
+          Створити проект</label>
       </div>
     );
   }
