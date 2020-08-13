5606f424d5d35e4bb3b5ecdccc64dc2dd18974ac
Andrii Kulyk <kyluk5@gmail.com>
2020-08-13 22:15:26 +0300

    fixing problem with background

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,7 @@ const App = () => {
       <div className={styles.container}>
         <Header />
         {/* <Registration /> */}
-        {/* <Login /> */}
+        <Login />
         {/* <SprintPage /> */}
       </div>
     </div>
--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -4,35 +4,33 @@ import styles from "./RegistrationForm.module.css";
 
 const RegistrationForm = () => {
   return (
-    <div className={styles.registration_form_wrapper}>
-      <form className={styles.registaration_form}>
-        <h1 className={styles.registaration_title}>Реєстрація</h1>
-        <input
-          type="email"
-          placeholder="E-mail"
-          className={styles.registaration_input}
-        />
-        <input
-          type="password"
-          placeholder="Пароль"
-          className={styles.registaration_input}
-        />
-        <input
-          type="password"
-          placeholder="Повторіть пароль"
-          className={styles.registaration_input}
-        />
-        <button type="submit" className={styles.registration_btn}>
-          Зареєструватися
-        </button>
-        <p className={styles.account}>
-          Маєте акаунт?{" "}
-          <NavLink className={styles.account_link} to="/">
-            Увійти
-          </NavLink>
-        </p>
-      </form>
-    </div>
+    <form className={styles.registaration_form}>
+      <h1 className={styles.registaration_title}>Реєстрація</h1>
+      <input
+        type="email"
+        placeholder="E-mail"
+        className={styles.registaration_input}
+      />
+      <input
+        type="password"
+        placeholder="Пароль"
+        className={styles.registaration_input}
+      />
+      <input
+        type="password"
+        placeholder="Повторіть пароль"
+        className={styles.registaration_input}
+      />
+      <button type="submit" className={styles.registration_btn}>
+        Зареєструватися
+      </button>
+      <p className={styles.account}>
+        Маєте акаунт?{" "}
+        <NavLink className={styles.account_link} to="/">
+          Увійти
+        </NavLink>
+      </p>
+    </form>
   );
 };
 
