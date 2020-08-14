5321079f4ec01385065331d0300eb4f5df792073
Andrii Kulyk <kyluk5@gmail.com>
2020-08-14 18:23:00 +0300

    SBC-03 Done

--- src/redux/reducers/registrationReducer.js
+++ src/redux/reducers/registrationReducer.js
@@ -1,14 +1,8 @@
 import { createReducer, combineReducers } from "@reduxjs/toolkit";
 import { setUserData } from "../actions/registarationAction";
 
-const email = createReducer("", {
-  [setUserData]: (_, { payload }) => payload.user.email,
+const auth = createReducer("", {
+  [setUserData]: (_, { payload }) => payload,
 });
-
-const uid = createReducer("", {
-  [setUserData]: (_, { payload }) => payload.user.uid,
-});
-
-const auth = combineReducers({ email, uid });
 
 export default auth;
