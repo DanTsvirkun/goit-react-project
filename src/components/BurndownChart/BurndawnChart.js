3fb83d95b7f8056189cf2dbe0397051207a3b326
Andrii Kochmaruk <luckyanry@gmail.com>
2020-08-14 13:42:08 +0200

    added graphic-btn

---
+++ src/components/BurndownChart/BurndawnChart.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styles from "./BurndownChart.module.css";
+
+class BurndawnChart extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default BurndawnChart;
---
+++ src/components/BurndownChartModalWindow/BurndownChartModalWindow.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./BurndownChartModalWindow.css";
+
+const BurndownChartModalWindow = () => {
+  return <div></div>;
+};
+
+export default BurndownChartModalWindow;
