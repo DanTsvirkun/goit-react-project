4adcf5507203395dc51f60ae57dfb13ade283be7
Andrii Kulyk <kyluk5@gmail.com>
2020-08-14 13:36:30 +0300

    problem with noth seriziable value in action

--- src/redux/operations/RegistarationOperation.js
+++ src/redux/operations/RegistarationOperation.js
@@ -1,6 +1,17 @@
 import { auth } from "../../config";
+import { loaderOn, loaderOff } from "../actions/loaderActions";
+import { errorOn } from "../actions/errorActions";
+import { setUserData } from "../actions/registarationAction";
 
-export const Registration = async ({ email, password }) => {
-  const result = await auth.createUserWithEmailAndPassword(email, password);
-  console.log(result);
+export const Registration = ({ email, password }) => async (dispatch) => {
+  try {
+    dispatch(loaderOn());
+    const result = await auth.createUserWithEmailAndPassword(email, password);
+    console.log(result);
+    dispatch(setUserData(result));
+  } catch (error) {
+    dispatch(errorOn(error));
+  } finally {
+    dispatch(loaderOff());
+  }
 };
