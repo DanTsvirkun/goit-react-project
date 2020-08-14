d83bb0be4323423609adfb77cbc4d4b47b43c7c7
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-08-14 19:27:07 +0300

    sprint creator modal

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,7 +13,7 @@ const App = () => {
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <Header />
-        {/* <ProjectPage /> */}
+        <ProjectPage />
         {/* <Projects /> */}
         {/* <SprintPage /> */}
         {/* <Registration /> */}
