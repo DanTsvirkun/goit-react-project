5d1751e04ce676e12109e375f37c9f45384d938c
Andrii Kochmaruk <luckyanry@gmail.com>
2020-08-14 13:42:08 +0200

    added graphic-btn

---
+++ src/components/BurndownChartBtn/BurndownChartBtn.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./BurndownChartBtn.module.css";
+
+const BurndownChartBtn = () => {
+  return <button className={styles.graphicBtn}></button>;
+};
+
+export default BurndownChartBtn;
