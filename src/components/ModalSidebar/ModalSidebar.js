9c29fec7d89c87d6a034855fc66b456adcb55325
Oscolok <oscolok7@gmail.com>
2020-08-17 11:41:22 +0300

    add all markup

--- src/components/ModalSidebar/ModalSidebar.js
+++ src/components/ModalSidebar/ModalSidebar.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import css from './ModalSidebar.module.css'
-
+import closeBtn from './closeBtn.svg'
+ 
 const ModalSidebar = ({ children, status, onClose }) => {
   return (
     <div className={css.modal__container}>
       <div className={css.modal__overlay}>
         <div className={css.modal__sidebar}>
+          <img className={css.modal__buttonClose} src={closeBtn} width="18" height="18"/>
           {children}
-          <button>Готово</button>
-          <button>Відміна</button>
+          <button className={css.modal__buttonReady}>Готово</button>
+          <button className={css.modal__buttonCanceling}>Відміна</button>
         </div>
       </div>
     </div>
