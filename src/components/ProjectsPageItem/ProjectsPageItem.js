6584bedbbd322cd7bd4b075f42a5296da0b9ddff
Mypaxa4e <ivanshtypula@gmail.com>
2020-08-17 17:56:19 +0300

    SBC-19

--- src/components/ProjectsPageItem/ProjectsPageItem.js
+++ src/components/ProjectsPageItem/ProjectsPageItem.js
@@ -1,21 +1,36 @@
 import React from "react";
+import { connect } from "react-redux";
+import projectsOperations from "../../redux/operations/projectsOperations.js";
 
 import itemStyle from "./ProjectsPageItem.module.css";
 
 import imgDel from "./projectImg/delete.svg";
 
-const ProjectsPageItem = ({ title, description }) => {
+const ProjectsPageItem = ({project: { title, description, id}, deleteProject }) => {
   return (
     <>
       <li className={itemStyle.item}>
         <h2 className={itemStyle.itemTitle}>{title}</h2>
-        <p className={itemStyle.itemDescription}>
-         {description}
-        </p>
-        <button type="button" className={itemStyle.itemTrashBtn}></button>
+        <p className={itemStyle.itemDescription}>{description}</p>
+        <button
+          type="button"
+          onClick={deleteProject}
+          className={itemStyle.itemTrashBtn}
+          id={id}
+        ></button>
       </li>
     </>
   );
 };
 
-export default ProjectsPageItem;
+const mapStateToProps = (state, {id}) => {
+  const project = state.projects.find((item) => item.id === id)  
+  return project ;
+  
+};
+
+const mapDispatchToProps = (dispatch) => ({
+  deleteProject: (e) => dispatch(projectsOperations.deleteProjectOperation(e)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProjectsPageItem);
