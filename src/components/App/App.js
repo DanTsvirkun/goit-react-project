ab866098e6c2ff26eae48bfc8694e06893224062
Andrii Kochmaruk <luckyanry@gmail.com>
2020-08-18 22:31:52 +0200

    added logic for drawing red line SBC-44

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,7 +16,7 @@ const App = () => {
       <div className={styles.container}>
         <Header />
         {/* <ProjectPage /> */}
-        <SprintPage />
+        {/* <SprintPage /> */}
         {/* <Registration /> */}
         {/* <Registration /> */}
         {/* <ProjectSidebar /> */}
