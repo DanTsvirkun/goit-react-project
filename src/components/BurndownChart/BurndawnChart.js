069af8177a50cecc040e2286199c84244af5299f
Andrii Kochmaruk <luckyanry@gmail.com>
2020-08-18 22:31:52 +0200

    added logic for drawing red line SBC-44

--- src/components/BurndownChart/BurndawnChart.js
+++ src/components/BurndownChart/BurndawnChart.js
@@ -1,29 +1,47 @@
 import React, { useEffect, useState } from "react";
+import { connect } from "react-redux";
 import { Line } from "react-chartjs-2";
+import moment from "moment";
 import styles from "./BurndownChart.module.css";
+import { itemsSelector } from "../../redux/selectors/TasksSelectors";
 
-const BurndawnChart = () => {
+const BurndawnChart = ({ hoursPlanned, hoursWastedPerDay, chartDays }) => {
   const [chartData, setChartData] = useState({});
+  const duration = hoursWastedPerDay - 1;
+  const sprinHoursPerDay = hoursPlanned / duration;
+  console.log(chartDays);
+
+  const getArrey = () => {
+    const result = [hoursPlanned];
+    let currentHours = hoursPlanned;
+    for (let i = 0; i < duration; i += 1) {
+      result.push((currentHours - sprinHoursPerDay).toFixed(1));
+      currentHours -= sprinHoursPerDay;
+    }
+    return result;
+  };
+
+  // const getData = () => {
+  //   const result = [];
+  //   for (let i = 0; i < hoursWastedPerDay; i += 1) {
+  //     result.push(moment(chartDays[0]).add(0, "day").format("dd"));
+  //   }
+  //   return result;
+  // };
+
+  // const daysFormat = moment().format("DD.MM");
+  // console.log(getData());
 
   const chart = () => {
     setChartData({
-      labels: [
-        "12 Aug",
-        "13 Aug",
-        "14 Aug",
-        "17 Aug",
-        "18 Aug",
-        "19 Aug",
-        "20 Aug",
-        "21 Aug",
-      ],
+      labels: [...chartDays],
       datasets: [
         {
           label: "Актуальний залишок трудовитрат в годинах",
           backgroundColor: "transparent",
           borderColor: "rgb(255, 0, 0)",
           borderWidth: 2,
-          data: [151, 129, 107, 86, 64, 43, 21, 0],
+          data: getArrey(),
         },
         {
           label: "Запланований залишок трудовитрат в годинах",
@@ -84,7 +102,20 @@ const BurndawnChart = () => {
   );
 };
 
-export default BurndawnChart;
+const mapStateToProps = (state, ownProps) => ({
+  hoursPlanned: itemsSelector(state)
+    .map((task) => Number(task.hoursPlanned))
+    .reduce((acc, taskValue) => {
+      return (acc += taskValue);
+    }, 0),
+  hoursWastedPerDay: itemsSelector(state)[0].hoursWastedPerDay.length,
+  chartDays: itemsSelector(state)[0].hoursWastedPerDay.map((task) => {
+    // const daysFormat = moment(task.currentDay);
+    return task.currentDay;
+  }),
+});
+
+export default connect(mapStateToProps)(BurndawnChart);
 
 // class BurndawnChart extends Component {
 //   state = {
