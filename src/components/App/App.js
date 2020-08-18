0168ac19c961128db25c381ec9230110e8a1e39a
DanTsvirkun <danila.tsvirkun555@gmail.com>
2020-08-18 13:49:36 +0300

    before checking sbc-19

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,7 +14,7 @@ const App = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        {/* <Header /> */}
+        <Header />
         {/* <ProjectPage /> */}
         {/* <SprintPage /> */}
         {/* <Registration /> */}
