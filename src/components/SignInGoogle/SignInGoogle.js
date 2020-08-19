f3478cdd979fb2cd2db1984e709d42ae17d2d125
Andrii Kulyk <kyluk5@gmail.com>
2020-08-19 11:44:10 +0300

    init

---
+++ src/components/SignInGoogle/SignInGoogle.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignInGoogle = () => {
+  return (
+    <p>
+      або<button>Sign In using Google Acount</button>
+    </p>
+  );
+};
+
+export default SignInGoogle;
