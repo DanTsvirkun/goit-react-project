5e06a9b189e2e069929f221e0600bd635d39cd72
Andrii Kulyk <kyluk5@gmail.com>
2020-08-20 22:12:05 +0300

    changed reaction if inputs

--- src/redux/reducers/loaderReducers.js
+++ src/redux/reducers/loaderReducers.js
@@ -1,16 +1,7 @@
-import {
-  createReducer
-} from "@reduxjs/toolkit";
-import {
-  loaderOn,
-  loaderOff
-} from "../actions/loaderActions";
+import { createReducer } from "@reduxjs/toolkit";
+import { loaderOn, loaderOff } from "../actions/loaderActions";
 
 export const loader = createReducer(false, {
   [loaderOn]: () => true,
   [loaderOff]: () => false,
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 09a0d97f097524cfdd4c25921df954eb1f6a7335
