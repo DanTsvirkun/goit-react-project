701e0ad19703e38bcfc6646c0f36094e390ea6f0
DanTsvirkun <danila.tsvirkun555@gmail.com>
2020-08-20 23:24:03 +0300

    finished

--- src/components/MemberList/MemberList.js
+++ src/components/MemberList/MemberList.js
@@ -1,21 +1,58 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { useSelector, connect } from "react-redux";
+import { useLocation } from "react-router-dom";
 import css from "./MembersList.module.css";
+import { updateMembers } from "../../redux/operations/membersOperations";
+import projects from "../../redux/operations/projectsOperations";
 
-const MemberList = () => {
-  const history = useHistory()
-  const members = useSelector(state => state.projects)
-  const findProject = members.find(member => history.location.pathname.includes(member.id));
+const MemberList = ({ updateMembers, getProjects, email }) => {
+  const location = useLocation();
+  const projects = useSelector((state) => state.projects);
+  const foundProject = projects.find((project) =>
+    location.pathname.includes(project.id)
+  );
+
+  async function deleteMember({ target }) {
+    const memberToDelete = target.dataset.email;
+    if (memberToDelete !== email) {
+      const newMembers = foundProject.members.filter(
+        (member) => member !== memberToDelete
+      );
+      await updateMembers(newMembers, location.pathname.split("/")[2]);
+      getProjects(email);
+    } else return;
+  }
 
   return (
     <>
-      <p className={css.allMembersLength}>Всього участникiв у проектi - {findProject ? findProject.members.length : "0"}</p>
+      <p className={css.allMembersLength}>
+        Всього участникiв у проектi -{" "}
+        {foundProject ? foundProject.members.length : "0"}
+      </p>
       <ul className={css.membersList}>
-        {findProject && findProject.members.map(email => <li key={email} className={css.membersItem}>{email} <button className={css.deleteButton}>X</button></li>)}
+        {foundProject &&
+          foundProject.members.map((email) => (
+            <li key={email} className={css.membersItem}>
+              <p>{email}</p>
+              <button
+                onClick={deleteMember}
+                data-email={email}
+                className={css.deleteButton}
+              ></button>
+            </li>
+          ))}
       </ul>
     </>
   );
 };
 
-export default MemberList;
+const mapStateToProps = (state) => ({
+  email: state.auth.email,
+});
+
+const mapDispatchToProps = {
+  updateMembers,
+  getProjects: projects.getProjectsByEmailOperationCustom,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MemberList);
