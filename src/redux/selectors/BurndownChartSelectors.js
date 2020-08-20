fb953076e03d0472962626d7a3a6cd68f9bcfd51
Andrii Kochmaruk <luckyanry@gmail.com>
2020-08-20 17:36:50 +0200

    finished task SBC-46

--- src/redux/selectors/BurndownChartSelectors.js
+++ src/redux/selectors/BurndownChartSelectors.js
@@ -1,6 +1,10 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { itemsSelector } from "./TasksSelectors";
 
+export const chartDaysSelector = createSelector([itemsSelector], (items) =>
+  items[0].hoursWastedPerDay.map((task) => task.currentDay)
+);
+
 export const hoursPlannedSelector = createSelector([itemsSelector], (items) =>
   items
     .map((task) => Number(task.hoursPlanned))
@@ -9,6 +13,10 @@ export const hoursPlannedSelector = createSelector([itemsSelector], (items) =>
     }, 0)
 );
 
-export const chartDaysSelector = createSelector([itemsSelector], (items) =>
-  items[0].hoursWastedPerDay.map((task) => task.currentDay)
+export const sumHoursWastedSelector = createSelector([itemsSelector], (items) =>
+  items
+    .map((task) => Number(task.hoursWasted))
+    .reduce((acc, taskValue) => {
+      return (acc += taskValue);
+    }, 0)
 );
