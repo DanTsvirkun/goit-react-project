15cae3e213c1052eeb3c8cf25aef296a3d9b9083
Oleksandr Tril <tril.oleksandr@gmail.com>
2020-08-20 15:44:27 +0300

    fixing problem

--- src/redux/operations/SprintOperation.js
+++ src/redux/operations/SprintOperation.js
@@ -30,24 +30,25 @@ export const addSprintOperation = (sprint) => async (dispatch) => {
   }
 };
 
-// export const getSprintsOperation = () => async (dispatch) => {
-//   try {
-//     dispatch(loaderOn());
-//     const result = await db.collection("sprints").get();
-//     const answer = result.docs.map((doc) => ({
-//       ...doc.data(),
-//       id: doc.id,
-//     }));
-//     dispatch(getSprints(answer));
-//   } catch (error) {
-//     dispatch(errorOn());
-//   } finally {
-//     dispatch(loaderOff());
-//   }
-// };
+export const getSprintsOperation = () => async (dispatch) => {
+  try {
+    dispatch(loaderOn());
+    const result = await db.collection("sprints").get();
+    const answer = result.docs.map((doc) => ({
+      ...doc.data(),
+      id: doc.id,
+    }));
+    dispatch(getSprints(answer));
+  } catch (error) {
+    dispatch(errorOn());
+  } finally {
+    dispatch(loaderOff());
+  }
+};
 
 export const getSprintByProjectId = (key) => async (dispatch) => {
   try {
+    dispatch(errorOff());
     dispatch(loaderOn());
     const result = await db
       .collection("sprints")
@@ -57,7 +58,6 @@ export const getSprintByProjectId = (key) => async (dispatch) => {
       ...doc.data(),
       id: doc.id,
     }));
-    console.log("answer", answer);
     dispatch(getSprints(answer));
   } catch (error) {
     dispatch(errorOn());
@@ -70,6 +70,7 @@ export const deleteSprintsOperation = ({ target: { id } }) => async (
   dispatch
 ) => {
   try {
+    dispatch(errorOff());
     dispatch(loaderOn());
     const result = await db.collection("sprints").doc(id).delete();
     dispatch(deleteSprints(id));
