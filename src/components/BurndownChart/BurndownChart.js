94a8d43c9d07d822d99aa81be61b11b47be7257d
Andrii Kochmaruk <luckyanry@gmail.com>
2020-08-21 01:57:44 +0200

    finished styling of burndown chart SBC-46new

--- src/components/BurndownChart/BurndownChart.js
+++ src/components/BurndownChart/BurndownChart.js
@@ -5,7 +5,6 @@ import { itemsSelector } from "../../redux/selectors/TasksSelectors";
 import {
   chartDaysSelector,
   hoursPlannedSelector,
-  sumHoursWastedSelector,
 } from "../../redux/selectors/BurndownChartSelectors";
 import {
   formatDateChanger,
@@ -16,7 +15,6 @@ import styles from "./BurndownChart.module.css";
 
 const BurndownChart = ({
   hoursPlanned,
-  hoursWasted,
   sprintDuration,
   chartDays,
   itemsArr,
@@ -28,63 +26,129 @@ const BurndownChart = ({
       labels: formatDateChanger(chartDays),
       datasets: [
         {
-          label: "Актуальний залишок трудовитрат в годинах",
-          backgroundColor: "transparent",
+          label: "Актуальний залишок трудовитрат",
+          fill: false,
           borderColor: "rgb(255, 0, 0)",
-          borderWidth: 2,
+          backgroundColor: "rgb(255, 0, 0)",
           data: getRedLineArr(hoursPlanned, sprintDuration),
         },
         {
-          label: "Запланований залишок трудовитрат в годинах",
+          label: "Запланований залишок трудовитрат",
+          fill: false,
           borderColor: "rgb(0, 89, 255)",
-          backgroundColor: "transparent",
-          borderWidth: 2,
-          data: getBlueLineArr(
-            hoursPlanned,
-            sprintDuration,
-            itemsArr,
-            hoursWasted
-          ),
+          backgroundColor: "rgb(0, 89, 255)",
+          data: getBlueLineArr(hoursPlanned, sprintDuration, itemsArr),
         },
       ],
     });
+  };
+
+  const chartOptions = {
+    layout: {
+      padding: {
+        left: 0,
+        right: 10,
+        top: 20,
+        bottom: 10,
+      },
+    },
+    responsive: true,
+    title: {
+      display: true,
+      text: "Burndown Chart (Calendar Team)",
+      fontColor: "#181C27",
+      fontFamily: "'Montserrat', 'sans-serif'",
+      fontSize: 20,
+      padding: 0,
+    },
+    elements: {
+      line: {
+        tension: 0.3, // disables bezier curves
+        borderWidth: 2,
+      },
+      point: {
+        pointStyle: "circle",
+        borderWidth: 2,
+        hoverRadius: 5,
+        hoverBackgroundColor: "rgba(255, 255, 255, 0.2)",
+        hoverBorderWidth: 2,
+        radius: 2,
+        hitRadius: 10,
+      },
+    },
+    tooltips: {
+      mode: "index",
+      titleFontSize: 14,
+      titleMarginBottom: 10,
+      bodyFontFamily: "'Montserrat', 'sans-serif'",
+      bodyFontSize: 14,
+      bodySpacing: 5,
+      bodyAlign: "center",
+      xPadding: 8,
+      yPadding: 8,
+      caretPadding: 5,
+      caretSize: 10,
+      cornerRadius: 5,
+      callbacks: {
+        label: (tooltipItem) => {
+          let label = tooltipItem.value;
+          label = "  " + label;
+          return label;
+        },
+      },
+    },
+    scales: {
+      yAxes: [
+        {
+          scaleLabel: {
+            display: true,
+            labelString: "Людино-години",
+            fontFamily: "'Montserrat', 'sans-serif'",
+            fontSize: 14,
+            fontColor: "#181C27",
+          },
+          ticks: {
+            beginAtZero: true,
+            fontSize: 14,
+            fontColor: "#181C27",
+          },
+          gridLines: {
+            display: true,
+          },
+        },
+      ],
+      xAxes: [
+        {
+          gridLines: {
+            display: false,
+          },
+          ticks: {
+            beginAtZero: false,
+            fontSize: 14,
+            fontColor: "#181C27",
+          },
+        },
+      ],
+    },
+    legend: {
+      display: true,
+      fullWidth: false,
+      labels: {
+        fontColor: "#181C27",
+        fontFamily: "'Montserrat', 'sans-serif'",
+        fontSize: 14,
+        boxWidth: 5,
+        usePointStyle: true,
+        padding: 20,
+      },
+    },
   };
 
   useEffect(() => chart(), []);
 
   return (
     <div className={styles.graphicWrapper}>
-      <Line
-        options={{
-          responsive: true,
-          title: { text: "Burndown Chart (Calendar Team)", display: true },
-          scales: {
-            yAxes: [
-              {
-                scaleLabel: {
-                  display: true,
-                  labelString: "Людино-години",
-                  // fontFamily: "'Montserrat', 'sans-serif'",
-                },
-                ticks: {
-                  beginAtZero: true,
-                },
-                gridLines: {
-                  display: true,
-                },
-              },
-            ],
-            xAxes: [
-              {
-                gridLines: {
-                  display: false,
-                },
-              },
-            ],
-          },
-        }}
-        data={chartData}
-      />
+      <Line options={chartOptions} data={chartData} />
     </div>
   );
 };
@@ -94,7 +158,6 @@ const mapStateToProps = (state) => ({
   sprintDuration: itemsSelector(state)[0].hoursWastedPerDay.length,
   chartDays: chartDaysSelector(state),
   itemsArr: itemsSelector(state),
-  hoursWasted: sumHoursWastedSelector(state),
 });
 
 export default connect(mapStateToProps)(BurndownChart);
