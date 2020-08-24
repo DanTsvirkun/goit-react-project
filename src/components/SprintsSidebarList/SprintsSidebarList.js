6e2c4a4a130d8f288bd4cb83f6eece71fcd5ba32
PopeyeIT <igorserov28@gmail.com>
2020-08-25 01:27:26 +0300

    fixed sidebar

--- src/components/SprintsSidebarList/SprintsSidebarList.js
+++ src/components/SprintsSidebarList/SprintsSidebarList.js
@@ -1,40 +1,27 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
 import {
   NavLink,
   useParams,
   useRouteMatch,
   useHistory,
-} from "react-router-dom";
-import css from "./SprintsSidebarList.module.css";
+} from 'react-router-dom';
+import css from './SprintsSidebarList.module.css';
 const SprintsSidebarList = ({ sprints }) => {
   const params = useParams();
   const match = useRouteMatch();
   const history = useHistory();
 
-  const handleClickOnLi = ({ target }) => {
-    const id = target.dataset.id;
-    history.push(`/projects/${params.projectId}/sprints/${id}`);
-  };
-
   return (
-    <ul className={css["sprint__sidebar-list"]}>
-      {sprints.map((sprint) => (
-        <li
-          onClick={handleClickOnLi}
-          key={sprint.id}
-          className={css["sprint__sidebar-item"]}
-          data-id={sprint.id}
-        >
+    <ul className={css['sprint__sidebar-list']}>
+      {sprints.map(sprint => (
+        <li key={sprint.id} className={css['sprint__sidebar-item']}>
           <NavLink
             to={`/projects/${params.projectId}/sprints/${sprint.id}`}
-            activeClassName={css["sprint__sidebar-link--active"]}
-            className={css["sprint__sidebar-link"]}
-            data-id={sprint.id}
+            activeClassName={css['sprint__sidebar-link--active']}
+            className={css['sprint__sidebar-link']}
           >
-            {sprint.title.trim().length > 14
-              ? `${sprint.title.slice(0, 14)}...`
-              : `${sprint.title}`}
+            <span className={css.span}>{sprint.title}</span>
           </NavLink>
         </li>
       ))}
@@ -42,7 +29,7 @@ const SprintsSidebarList = ({ sprints }) => {
   );
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   sprints: state.sprints.items,
 });
 
